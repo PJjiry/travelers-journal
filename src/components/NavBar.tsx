82b1e57965b523fd1ec85b5107f454b0b4cf4c5f
Petr Jiránek <jiranekpetr5@gmail.com>
2024-03-30 11:01:51 +0100

    feat(FE): add basic markup and routing

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const NavBar:React.FC = () => {
+    return(<h1>Navbar</h1>)
+}
+export default NavBar
---
+++ src/pages/Main.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Main:React.FC = () => {
+  return(<h1>Main page</h1>)
+}
+export default Main
