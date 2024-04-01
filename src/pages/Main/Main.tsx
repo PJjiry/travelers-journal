2da26aafdaaec636fb46d57c320b1a28661ec200
Petr Jiránek <jiranekpetr5@gmail.com>
2024-04-01 23:02:10 +0200

    feat(FE): add places list component draft and styling to it

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import classes from './Main.module.css'
 import SearchBar from '../../components/SearchBar/SearchBar.tsx';
+import PlacesList from '../../components/PlacesList/PlacesList.tsx';
 
 const Main:React.FC = () => {
   return(
       <main className={classes.main}>
         <SearchBar />
+          <PlacesList/>
       </main>
   )
 }
--- src/theme.ts
+++ src/theme.ts
@@ -5,5 +5,9 @@ export const theme = {
         textLight: '#e6e6e6',
         textDark: '#000000',
         background: '#ffffff',
+        darkGrey: '#00000026',
+        cityColor: 'rgba(0, 123, 167, 1)',
+        natureColor: 'rgb(43, 155, 18)',
+        secondaryTransparent: 'rgba(190, 20, 235, 0.5)',
     },
 };
