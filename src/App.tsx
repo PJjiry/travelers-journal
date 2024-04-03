db65c6901fa4f559154c106cf0a22809b36b38cf
Petr Jiránek <jiranekpetr5@gmail.com>
2024-04-03 17:42:49 +0200

    feat(FE): edit styling of edit place site

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import {Route, Routes} from "react-router-dom"
 import PackingList from './pages/PackingList.tsx';
-import NewPlace from './pages/NewPlace.tsx';
+import NewPlace from './pages/NewPlace/NewPlace.tsx';
 import Main from './pages/Main/Main.tsx';
 import EditPlace from './pages/EditPlace/EditPlace.tsx';
 import About from './pages/About.tsx';
