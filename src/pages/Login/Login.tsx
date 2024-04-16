f84dffef2f10f42bd7a68125cabcb1d0993df2b0
Petr Jiránek <jiranekpetr5@gmail.com>
2024-04-16 18:40:25 +0200

    feat(FE): add sign up component and enable creating a user, add small menu in the header

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,8 +1,9 @@
 import {useEffect, useState} from 'react';
 import {auth} from '../../firebase';
-import {signInWithEmailAndPassword} from 'firebase/auth';
+import {signInWithEmailAndPassword, createUserWithEmailAndPassword} from 'firebase/auth';
 import classes from './Login.module.css';
 import {useNavigate} from 'react-router-dom';
+import {FirebaseError} from 'firebase/app';
 
 // Component for the login page...the first page the user sees when they visit the app
 const Login = () => {
@@ -19,6 +20,9 @@ const Login = () => {
     // state to manage the logged in status
     const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
 
+    // state to manage the mode of the login page (sign-up or login)
+    const [mode, setMode] = useState<string>('sign-up');
+
     // using the navigate hook to redirect to the main page
     const navigate = useNavigate();
 
@@ -29,6 +33,37 @@ const Login = () => {
             setIsLoggedIn(true); // Set isLoggedIn to true after successful login
         } catch (error) {
             setError('User does not exist or the password is incorrect.');
+        }
+    };
+
+    // function to sign up the user...it uses the email and password entered by the user, if the user already exists, it displays an error message
+    const signUp = async () => {
+        if (email === '' || password === '') {
+            setError('Please enter email and password.');
+            return;
+        }
+
+        try {
+            await createUserWithEmailAndPassword(auth, email, password); // Create a new user with the email and password (firebase function)
+            setIsLoggedIn(true); // Set isLoggedIn to true after successful sign up
+        } catch (error) {
+            const firebaseError = error as FirebaseError;
+            switch (firebaseError.code) {
+                case 'auth/email-already-in-use':
+                    setError('Email already in use.');
+                    break;
+                case 'auth/invalid-email':
+                    setError('Invalid email.');
+                    break;
+                case 'auth/operation-not-allowed':
+                    setError('Operation not allowed.');
+                    break;
+                case 'auth/weak-password':
+                    setError('Weak password. Enter at least 6 characters.');
+                    break;
+                default:
+                    setError('An unknown error occurred.');
+            }
         }
     };
 
@@ -45,23 +80,54 @@ const Login = () => {
         }
     }, [isLoggedIn, navigate]);
 
-    // render the login page with email and password input fields and a sign in button
+    // render the login page with email and password input fields and a sign in button if the mode is login otherwise render the sign up page with email and password input fields and a sign up button
     return (
         <div className={classes.loginPage}>
             <div className={classes.loginModal}>
-                <h2>Login</h2>
-                <input type="email" placeholder="Email"
-                       onChange={e => {
-                           setEmail(e.target.value)
-                           setError('')
-                       }}
-                />
-                <input type="password" placeholder="Password" onChange={e => {
-                    setPassword(e.target.value)
-                    setError('')
-                }}/>
-                <button onClick={signIn}>Sign In</button>
-                {error && <p>{error}</p>}
+                {mode === 'login' ? <><h2>Login</h2>
+                        <input type="email" placeholder="Email"
+                               onChange={e => {
+                                   setEmail(e.target.value)
+                                   setError('')
+                               }}
+                        />
+                        <input type="password" placeholder="Password" onChange={e => {
+                            setPassword(e.target.value)
+                            setError('')
+                        }}/>
+                        <button onClick={signIn}>Sign In</button>
+                        {error && <p>{error}</p>}
+                        <p className={classes.changeModeText}>You do not have any account? Sign up!</p>
+                        <button onClick={() => {
+                            setMode('sign-up')
+                            setError('')
+                        }
+                        }>Sign up
+                        </button>
+                    </>
+                    :
+                    <><h2>Sign Up</h2>
+                        <input type="email" placeholder="Email"
+                               onChange={e => {
+                                   setEmail(e.target.value)
+                                   setError('')
+                               }}
+                        />
+                        <input type="password" placeholder="Password" onChange={e => {
+                            setPassword(e.target.value)
+                            setError('')
+                        }
+                        }/>
+                        <button onClick={signUp}>Sign Up</button>
+                        {error && <p>{error}</p>}
+                        <p className={classes.changeModeText}>Do you already have an account? Login in to the app!</p>
+                        <button onClick={() => {
+                            setMode('login')
+                            setError('')
+                        }}>Login
+                        </button>
+                    </>
+                }
             </div>
         </div>
     );
